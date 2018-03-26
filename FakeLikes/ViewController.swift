ca6731e8d3110811348f62dfb3cf77ee78407fef
xwn <woshixwn@gmail.com>
2018-03-26 22:41:03 +0800

    Initial Commit

---
+++ FakeLikes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FakeLikes
+//
+//  Created by 徐炜楠 on 2018/3/26.
+//  Copyright © 2018年 徐炜楠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
